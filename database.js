443c32d02123169fb5b02166524cce215867cd18
Sara Mansouri <s.mansoury@gmail.com>
2021-03-25 17:56:04 -0700

    initial version of implementing Tags and Tasks

--- database.js
+++ database.js
@@ -1,9 +1,50 @@
 let Database = {
     cindy: {
-        reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}]
+        reminders: [
+            {
+            id: 1, 
+            title: " Reading", 
+            description: "abcabc",
+            location: "vancouver",  
+            completed: false, 
+            date:new Date(), 
+            tags: ["personal", "self-improvement"],
+            tasks:[ "reading"]         
+            },
+            {
+            id: 2, 
+            title: "Work ", 
+            description: "abcabc",
+            location: "vancouver", 
+            completed: false, 
+            date:new Date(), 
+            tags: ["work", "skills"],
+            tasks:[ "Learn Node"]        
+            }
+        ]
     },
     alex: {
-        reminders: []
+        reminders: [
+            {
+            id: 3, 
+            title: "alex Rading", 
+            description: "abcabc",
+            location: "vancouver", 
+            completed: false, 
+            date:new Date(), 
+            tags: ["family", "hoby"],
+            tasks:[ "reading"]         
+            },
+            {
+            id: 4, 
+            title: "alex Node", 
+            description: "abcabc", 
+            location: "vancouver", 
+            completed: false, 
+            date:new Date(), 
+            tags: ["work"],
+            tasks:[ "Learn Cooking"]        
+            }]
     } 
 }
 
