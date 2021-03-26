96577b3df9202a06eed3752a3b4cad2f21b5f2be
Miss J Hayek <jenniferanne.hayek@gmail.com>
2021-03-26 15:45:40 -0700

    trying to resolve

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -1,8 +1,6 @@
 let database = require("../database");
 var fs = require('fs')
 
-
-
 let remindersController = {
   list: (req, res) => {
     res.render("reminder/index", { reminders: database.cindy.reminders });
@@ -25,15 +23,10 @@ let remindersController = {
   },
 
   create: (req, res) => {
-
     let reminder = {
       id: database.cindy.reminders.length + 1,
       title: req.body.title,
       description: req.body.description,
-      date: req.body.date,
-      location: req.body.location,
-      tags: req.body.tags.split(","),
-      tasks: req.body.tasks.split(","),
       completed: false,
     };
     database.cindy.reminders.push(reminder);
@@ -68,6 +61,5 @@ let remindersController = {
 
 
 };
-
 
 module.exports = remindersController;
--- database.js
+++ database.js
@@ -26,7 +26,6 @@ let Database = {
     alex: {
         reminders: [
             {
-<<<<<<< HEAD
                 id: 3,
                 title: "alex Rading",
                 description: "abcabc",
@@ -45,26 +44,6 @@ let Database = {
                 date: new Date(),
                 tags: ["work"],
                 tasks: ["Learn Cooking"]
-=======
-            id: 100, 
-            title: "alex Rading", 
-            description: "abcabc",
-            location: "vancouver", 
-            completed: false, 
-            date:new Date(), 
-            tags: ["family", "hoby"],
-            tasks:[ "reading"]         
-            },
-            {
-            id: 101, 
-            title: "alex Node", 
-            description: "abcabc", 
-            location: "vancouver", 
-            completed: false, 
-            date:new Date(), 
-            tags: ["work"],
-            tasks:[ "Learn Cooking"]        
->>>>>>> fde6834d70c35239d1ed3a176625252d4b582574
             }]
     }
 }
